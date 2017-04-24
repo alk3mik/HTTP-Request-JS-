01dd6f55808aafe47984302cf51c051f8fb22d9e
Massimiliano Porrini <mozz76@gmail.com>
2017-04-24 22:02:47 +0200

    Improvements

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ var forecastContainer = document.getElementById("forecast");
 
 var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function(event) {
+
+	event.preventDefault();
 
 	var xhr = new XMLHttpRequest();
 	// var url = "http://localhost:3000/posts";
 	// var url = "http://openweathermap.org/api";
-	var url = "http://api.openweathermap.org";
+	var url = "http://api.openweathermap.org/data/2.5/weather";
 	///data/2.5/weather?q=London&appid=e94f99a51481de0a1ca44714266f918c";
 
 	//var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e94f99a51481de0a1ca44714266f918c";
@@ -74,6 +76,14 @@ btn.addEventListener("click", function() {
 
 	if (xhr) {
 
+		var cityName = '?q=' + document.getElementById("city-name").value;
+		var apiKey = '&appid=' + document.getElementById("appid").value;
+
+		xhr.open('GET', url + cityName + apiKey, true);
+		xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+		
+
+
 		xhr.onreadystatechange = function() {
 
 	// The *readyState* method of XMLHttpRequest can return 5 values, numbered as 0, 1, 2, 3, 4 .
@@ -83,7 +93,7 @@ btn.addEventListener("click", function() {
 
 	//    			if (xhr.status === 200) {
 	// But, if one wants to include all the error codes one has:
-				if ((xhr.status >= 200 && xhr.status <= 300) || xhr.status === 304) {
+				if ((xhr.status >= 200 && xhr.status <= 300) || xhr.status == 304) {
 
 	// The JavaScript function JSON.parse(text) can be used to convert a JSON text into a JavaScript object:
 
@@ -104,7 +114,7 @@ btn.addEventListener("click", function() {
 
 		};
 
-	xhr.open('GET', url, true);
+	
 	xhr.send();
 
 	}
